25b476a0f5910f36186cee4fac7559896b496216
minsoku <minsoku03@gmail.com>
2024-10-28 16:59:56 +0900

    console.log 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log('iframe페이지', document.cookie);
   return (
     <div className={styles.page}>
-      iframe 테스트 2번
+      iframe 페이지
       <iframe
         src="https://minsoku.shop"
+        // src="http://localhost:3001"
         width="100%"
         height="500"
-        allowfullscreen
-        sandbox
+        allowFullScreen
       />
     </div>
   );
